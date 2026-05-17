417f41a719d2f2eac6dc45f54eeb2d42e13ac01f
sone <atsusone123@gmail.com>
2026-05-17 17:07:59 +0900

    feat: ページの作成に関するページの追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,15 @@ export default function Home() {
     <>
       <h1>Notes on Next.js</h1>
       <p>このウェブサイトはNext.jsとその周辺情報に関するノートをまとめたものである。</p>
+      <p>すべてのリソースは<a href="https://github.com/sone0123/nextjspractice" target="_blank" rel="noopener noreferrer">GitHub</a>にある。</p>
       <h2>ノート</h2>
       <ul>
-        <li><Link href="/rules">フォルダとファイルの規則</Link></li>
         <li>React</li>
+        <li><Link href="/rules">フォルダとファイルの規則</Link></li>
+        <li><Link href="/pages">ページの作成</Link></li>
       </ul>
       <h2>参考文献</h2>
+      <p>このウェブサイトの内容は主に次のウェブサイトの内容に基づいている。</p>
       <ul>
         <li><a href="https://nextjsjp.org/docs" target="_blank" rel="noopener noreferrer">Next.js 日本語ドキュメント</a></li>
       </ul>
